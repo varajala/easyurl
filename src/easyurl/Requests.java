51be707e4e6e61c15822e3af406f2f3d7617d150
Valtteri Rajalainen <rajalainen.valtteri@gmail.com>
2021-03-06 09:41:01 +0200

    Started working on sending POST requests

--- src/easyurl/Requests.java
+++ src/easyurl/Requests.java
@@ -17,7 +17,7 @@ public class Requests {
      * @throws RequestFailedException -
      */
     public static void get(String url, String filepath) throws RequestFailedException {
-        try (InputStream data = connect(makeURL(url));
+        try (InputStream data = connectAndGetInputStream(makeURL(url));
              FileOutputStream file = openFileStream(filepath))
         {
             writeDataToFile(data, file);
@@ -28,6 +28,34 @@ public class Requests {
             throw new RequestFailedException(info);
         }
     }
+    
+    /**
+     * @param url URL as a String.
+     * @param JSONString Key and value pairs representing the data to be sent.
+     * @throws RequestFailedException -
+     */
+    public static void post(String url, String JSONString) throws RequestFailedException {
+        try {
+            byte[] requestJSON = JSONString.getBytes();
+            int lengthOfData = requestJSON.length;
+            HttpURLConnection connection = makeHTTPConnection(makeURL(url));
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Length", Integer.toString(lengthOfData));
+            connection.setRequestProperty("Content-Type", "application/json; charset=UTF-8");
+            connection.setDoOutput(true);
+            connection.connect();
+            try (OutputStream outputStream = connection.getOutputStream()) {
+                outputStream.write(requestJSON);
+            }
+        } catch (ProtocolException e) {
+            String info = "POST is not allowed";
+            throw new RequestFailedException(info);
+        } catch (IOException e) {
+            String info = "Connection failed";
+            throw new RequestFailedException(info);
+        }
+    }
+    
     
     private static URL makeURL(String urlString) {
         URL url = null;
@@ -41,7 +69,7 @@ public class Requests {
     }    
     
     
-    private static InputStream connect(URL url) {
+    private static InputStream connectAndGetInputStream(URL url) {
         InputStream stream = null;
         try {
             stream = new BufferedInputStream(url.openConnection().getInputStream());
@@ -50,6 +78,18 @@ public class Requests {
             throw new RequestFailedException(info);
         }
         return stream;
+    }
+    
+    
+    private static HttpURLConnection makeHTTPConnection(URL url) {
+        HttpURLConnection httpConnection = null;
+        try {
+           httpConnection =  (HttpURLConnection) url.openConnection();
+        } catch (IOException e) {
+            String info = "Connection failed";
+            throw new RequestFailedException(info);
+        }
+        return httpConnection;
     }
     
     
